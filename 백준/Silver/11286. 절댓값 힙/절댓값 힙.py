2f3149a6f3acc64c8c6a4a878d0ae0d8dc1ff67b
Seungyoup Hyun <48678866+hsy5830@users.noreply.github.com>
2023-03-20 15:28:25 +0900

    [Silver I] Title: 절댓값 힙, Time: 156 ms, Memory: 38936 KB -BaekjoonHub

---
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.py
@@ -0,0 +1,22 @@
+'''
+절댓값 힙 : https://www.acmicpc.net/problem/11286
+'''
+import sys
+# print = sys.stdout.write
+input = sys.stdin.readline
+import heapq
+
+N = int(input())
+heap = []
+
+for i in range(N):
+    x = int(input())
+    if x != 0:
+        if x>0: heapq.heappush(heap, (x, 1))
+        else: heapq.heappush(heap, (-x, -1))
+    else:
+        if len(heap) == 0:
+            print(0)
+            continue
+        r = heapq.heappop(heap)
+        print(r[0] * r[1])
