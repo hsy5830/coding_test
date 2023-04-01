ae16ccc9565f7f2a1815c0e78c017acb41b257c5
Seungyoup Hyun <48678866+hsy5830@users.noreply.github.com>
2023-04-01 16:27:32 +0900

    [Gold V] Title: ABCDE, Time: 1636 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.py
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -14,45 +14,32 @@ visited = [False] * N
 exist = [0]
 
 def DFS(v, depth):
+    # 방문하면 visited, depth 업데이트
     visited[v] = True
     depth += 1
-    # print(visited, depth)
+    
+    # 만약 depth == 5 가 되면 끝. 종료
     if depth == 5:
         exist[0] = 1
         return
-    
-    # 원래는 인접한 노드를 다 접근하고 visited 체크한다. 하지만,
-    # 여기선 한 줄기에 대한 DFS의 깊이를 찾아야하기 때문에 인접한 노드 한 개만 먼저 접근.
-    # 1 -> 2, 3 일 때 DFS(1) 에서 2, 3을 visited 하지 않고, 2(or 3) 먼저 접근한다.
-    # if len(friends[v]) != 0:
-    #     p = friends[v].pop()
-    #     if not visited[p]:
-    #         DFS(p, depth)
-    #         visited[p] = False
 
-    # for p in friends[v]:
-    #     if not visited[p]:
-    #         DFS(p, depth)
-    #         visited[p] = False
-
+    # p를 시작점으로 탐색한 뒤, 다시 p를 False로 돌려놓는다
+    # 다른 시작점에서 p를 방문할 수 있도록 하기 위함
+    # 1 -> 0 -> 3 방문하다가 끊겼다면, 3, 0, 1 순서로 다시 visited[p] = False가 적용될 것.
     for p in friends[v]:
         if not visited[p]:
             DFS(p, depth)
             visited[p] = False
 
+    visited[v] = False # DFS에서 시작점에 대해 False로 초기화해주진 않는다. (1)
 
-# for i in range(N):
-#     for j in friends[i]:
-#         if not visited[j]:
-#             DFS(i, 0)
-#             visited[j] = False
-#             if exist[0] == 1: break
-
+# 모든 시작점에서 탐색하기
 for i in range(N):
     if not visited[i]:
         DFS(i, 0)
-        visited[i] = False
+        # visited[i] = False # DFS에서 시작점에 대해 False로 초기화해주진 않는다. (2)
         if exist[0] == 1: break
 
+## (1) or (2) 둘 중 하나는 해줘야 함
 
 print(exist[0])
