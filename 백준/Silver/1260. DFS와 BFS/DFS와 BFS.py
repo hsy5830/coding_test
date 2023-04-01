b242daa111f5a83de49a155866cd3673e2bdc08e
Seungyoup Hyun <48678866+hsy5830@users.noreply.github.com>
2023-04-02 02:05:28 +0900

    [Silver II] Title: DFS와 BFS, Time: 468 ms, Memory: 34192 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.py
@@ -5,11 +5,15 @@ DFS와 BFS : https://www.acmicpc.net/problem/1260
 from collections import deque
 
 N, M, V = map(int, input().split())
-graph = {i:[] for i in range(1, N+1)}
-l = [tuple(map(int, input().split())) for _ in range(M)]
-l.sort(key = lambda x: (x[1], x[0]))
 
-for n1, n2 in l:
+# # 이렇게하면 작은 노드부터 방문하는데 문제가 생기는 거 같음
+# # 3 7 / 4 3 -> 3 : [7, 4] 가 들어가게 된다.
+# l = [tuple(map(int, input().split())) for _ in range(M)]
+# l.sort(key = lambda x: (x[1], x[0]))
+
+graph = {i:[] for i in range(1, N+1)}
+for _ in range(M):
+    n1, n2 = map(int, input().split())
     graph[n1].append(n2)
     graph[n2].append(n1)
 
@@ -30,12 +34,14 @@ def DFS(V):
 visited_bfs = [False] * (N+1)
 ans_bfs = []
 def BFS(V):
+    # V 방문 표시
     visited_bfs[V] = True
     ans_bfs.append(V)
 
     queue = deque([V])
     while len(queue):
         now = queue.popleft()
+        # now에 인접한 노드 차례대로 방문
         for n in graph[now]:
             if not visited_bfs[n]:
                 queue.append(n)
