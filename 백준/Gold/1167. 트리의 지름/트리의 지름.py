79a341b0bd50f36012d5e72cab76221e815717ec
Seungyoup Hyun <48678866+hsy5830@users.noreply.github.com>
2023-04-04 16:35:11 +0900

    [Gold II] Title: 트리의 지름, Time: 952 ms, Memory: 150648 KB -BaekjoonHub

---
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.py
@@ -0,0 +1,63 @@
+'''
+트리의 지름 : https://www.acmicpc.net/problem/1167
+'''
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+# tree 생성
+tree = {i:deque([]) for i in range(1, N+1)}
+for _ in range(N):
+    l = list(map(int, input().split()))
+    node = l[0]
+    idx = 1
+    while idx < len(l) - 1:
+        n = l[idx]
+        dist = l[idx+1]
+        tree[node].append((n, dist))
+        idx += 2
+
+
+# distance (거리 정보)
+random_start = 1
+M, M_node = 1, random_start
+
+queue = deque([(random_start, 0)]) # already deque
+visited = [False] * (N+1)
+visited[random_start] = True
+
+while len(queue):
+    now, dist = queue.popleft()
+    
+    next = tree[now]
+    for n, d in next:
+        if not visited[n]:
+            queue.append((n, d + dist))
+            visited[n] = True
+            if M <= d + dist:
+                M = d + dist
+                M_node = n
+
+
+
+queue = deque([(M_node, 0)]) # already deque
+visited = [False] * (N+1)
+visited[M_node] = True
+
+M2 = 1
+while len(queue):
+    now, dist = queue.popleft()
+    
+    next = tree[now]
+    for n, d in next:
+        if not visited[n]:
+            queue.append((n, d + dist))
+            visited[n] = True
+            if M2 <= d + dist:
+                M2 = d + dist
+                M_node = n
+
+print(M2)
+
