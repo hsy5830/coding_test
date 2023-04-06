f326b8d5c8d30ed79cd2d8299cfc91fe5d0088ef
Seungyoup Hyun <48678866+hsy5830@users.noreply.github.com>
2023-04-06 13:19:33 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 3840 ms, Memory: 34108 KB -BaekjoonHub

--- 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
@@ -1,13 +1,37 @@
+'''
+https://www.acmicpc.net/problem/1715
+
+Gold : 카드 정렬하기
+'''
+
 import heapq
 
-N = int(input())
-l = sorted([int(input()) for _ in range(N)])
+# ##### [1] #####
+# N = int(input())
+# l = sorted([int(input()) for _ in range(N)])
 
-if N == 1: print(0)
-else:
-    ans = 0
-    for _ in range(N-1):
-        tmp  = heapq.heappop(l) + heapq.heappop(l)
-        ans += tmp
-        heapq.heappush(l, tmp)
-    print(ans)
+# if N == 1: print(0)
+# else:
+#     ans = 0
+#     for _ in range(N-1):
+#         tmp  = heapq.heappop(l) + heapq.heappop(l)
+#         ans += tmp
+#         heapq.heappush(l, tmp)
+#     print(ans)
+
+
+##### [2] #####
+N = int(input())
+cards = [int(input()) for _ in range(N)]
+
+heapq.heapify(cards)
+
+ans = 0
+while len(cards) > 1:
+    c1 = heapq.heappop(cards)
+    c2 = heapq.heappop(cards)
+
+    ans += (c1 + c2)
+    heapq.heappush(cards, c1 + c2)
+
+print(ans)
