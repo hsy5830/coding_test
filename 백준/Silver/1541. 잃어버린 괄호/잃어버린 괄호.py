9bd34564084f07f3b22f26029576def5408fabed
Seungyoup Hyun <48678866+hsy5830@users.noreply.github.com>
2023-04-07 16:05:31 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -1,23 +1,34 @@
 '''
 잃어버린 괄호 : https://www.acmicpc.net/problem/1541
 '''
-s = input()
-numbers = []
-opers = []
 
-tmp = ''
-for i in range(len(s)):
-    if s[i].isdigit():
-        tmp += s[i]
-    else:
-        numbers.append(int(tmp))
-        tmp = ''
-        opers.append(s[i])
-numbers.append(int(tmp))
+# ## [1]
+# s = input()
+# numbers = []
+# opers = []
 
-if '-' not in opers:
-    print(sum(numbers))
-else:
-    idx = opers.index('-')
-    print(sum(numbers[:idx+1]) - sum(numbers[idx+1:]))
+# tmp = ''
+# for i in range(len(s)):
+#     if s[i].isdigit():
+#         tmp += s[i]
+#     else:
+#         numbers.append(int(tmp))
+#         tmp = ''
+#         opers.append(s[i])
+# numbers.append(int(tmp))
 
+# if '-' not in opers:
+#     print(sum(numbers))
+# else:
+#     idx = opers.index('-')
+#     print(sum(numbers[:idx+1]) - sum(numbers[idx+1:]))
+
+
+## [2]
+A = list(map(str, input().split('-')))
+
+ans = sum(list(map(int, A[0].split('+'))))
+for i in range(1, len(A)):
+    ans -= sum(list(map(int, A[i].split('+'))))
+
+print(ans)
