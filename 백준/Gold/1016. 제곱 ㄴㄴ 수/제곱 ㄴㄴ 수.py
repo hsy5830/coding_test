e3128093acd1a06b2b1e15d2f4ac681667510a29
Seungyoup Hyun <48678866+hsy5830@users.noreply.github.com>
2023-04-11 15:25:18 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 1256 ms, Memory: 79500 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.py
@@ -6,9 +6,12 @@ m, M = map(int, input().split())
 # nono = [True] * (M+1) # 이렇게 시작하면 메모리 초과
 nono = [i for i in range(m, M+1)]
 
-for i in range(2, M+1):
-    if i > M / i: break
-    k = max(int((m / i) // i), 1)
+# for i in range(2, M+1):
+for i in range(2, int(M**0.5)+1):
+    # if i > M / i: break # 위위 줄의 for문과 같이 썼는데 더 느리다.
+    # k = max(int((m / i) // i), 1) # m // (i * i)
+
+    k = m // (i * i)
     while k * i <= M / i:
         if k * i >= m / i:
             nono[k * i * i - m] = 0
