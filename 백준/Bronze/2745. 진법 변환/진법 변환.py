11c39e47ba5fc28a2a18018ae93b4af694fe412d
Seungyoup Hyun <48678866+hsy5830@users.noreply.github.com>
2024-11-23 18:24:51 +0900

    [Bronze II] Title: 진법 변환, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.py
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -1,14 +1,2 @@
-D = {}
-for i in range(26):
-    D[chr(65+i)] = i+10
-for i in range(10):
-    D[f"{i}"] = i
-
-N, B = input().split()
-B = int(B)
-
-ans = 0
-for i in range(len(N)-1, -1, -1):
-    ans += D[N[i]] * (B**(len(N)-1-i))
-
-print(ans)
+n,b=input().split()
+print(int(n,int(b)))
